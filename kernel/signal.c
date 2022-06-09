209ec6c41ba9a226b903adf14ecfc07792232117
Wei Mingzhi <whistler@member.fsf.org>
2022-06-09 14:11:09 +0800

    android: Enable building ashmem and binder as modules, taken from debian
    By: Ben Hutchings <ben@decadent.org.uk>

--- kernel/signal.c
+++ kernel/signal.c
@@ -1368,6 +1368,7 @@ struct sighand_struct *__lock_task_sighand(struct task_struct *tsk,
 
 	return sighand;
 }
+EXPORT_SYMBOL_GPL(__lock_task_sighand);
 
 /*
  * send signal info to all the members of a group
